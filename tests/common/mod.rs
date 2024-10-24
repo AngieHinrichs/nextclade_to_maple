dd3f5503b394dc96f6dd069cbf8f31f16b3f9aef
Angie Hinrichs <angie@soe.ucsc.edu>
2024-10-24 10:03:31 -0700

    Ignore newline differences (Windows "\r\n" vs. Linux/Mac "\n") when comparing test result files.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 pub fn compare_files(path1: &str, path2: &str) -> Result<(), Box<dyn Error>> {
     let content1 = fs::read_to_string(path1)?;
     let content2 = fs::read_to_string(path2)?;
+    let content2 = content2.replace("\r", "");
     assert_eq!(content1, content2);
     Ok(())
 }
